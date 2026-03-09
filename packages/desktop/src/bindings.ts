e31874a10749272482d64ead88f1923d509091c2
Sascha Gering <sascha.gering@exxeta.com>
2026-03-09 14:49:25 +0100

    chore: regenerate bindings with convertPptxToPdfCommand

--- packages/desktop/src/bindings.ts
+++ packages/desktop/src/bindings.ts
@@ -20,6 +20,7 @@ export const commands = {
 	resolveAppPath: (appName: string) => __TAURI_INVOKE<string | null>("resolve_app_path", { appName }),
 	openPath: (path: string, appName: string | null) => __TAURI_INVOKE<null>("open_path", { path, appName }),
 	convertXlsxToHtmlCommand: (path: string) => __TAURI_INVOKE<string>("convert_xlsx_to_html_command", { path }),
+	convertPptxToPdfCommand: (path: string) => __TAURI_INVOKE<number[]>("convert_pptx_to_pdf_command", { path }),
 };
 
 /** Events */
